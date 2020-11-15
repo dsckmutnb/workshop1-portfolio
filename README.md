d3606d55be15f8f9fdee2c57a21aed7911944cca
thanawatgulati <thanawatgulatijames@gmail.com>
2020-11-15 14:07:51 +0700

    Finish Demo

--- README.md
+++
@@ -1,3 +0,0 @@
-# js-kvmxpp
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-kvmxpp)
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-// Import stylesheets
-import "./style.css";
-import data from "./data.json";
-
 // Write Javascript code!
-const appDiv = document.getElementById("app");
-appDiv.innerHTML = `<h1 id="head">My Portfolio</h1>`;
-const portList = document.getElementById("port-list");
-
-data.forEach(r => {
-  const card = document.createElement("div");
-  card.className = "my-card";
-  card.innerHTML = `<img src="${r.image}" /><p><b>Project name: </b>${
-    r.title
-  }</p>`;
-  portList.appendChild(card);
-});
+async function main() {
+  const appDiv = document.getElementById("app");
+  appDiv.innerHTML = `<h1 id="head">My Portfolio</h1>`;
+  const portList = document.getElementById("port-list");
+  const data = await (await fetch("./data.json")).json();
+  data.forEach((r) => {
+    const card = document.createElement("div");
+    card.className = "my-card";
+    card.innerHTML = `<img src="${r.image}" /><p><b>Project name: </b>${
+      r.title
+    }</p><p><b>Description: </b>${r.description}</p>
+    </b><b>Lang: </b>${r.lang.map((l) => `<span class="lang">${l}</span>`)}`;
+    portList.appendChild(card);
+  });
+}
+main();
